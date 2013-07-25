2c423ea7489649cba5a3c54215547fe3664370cf
Shawn Campbell <scampbell@cashstar.com>
2013-07-25 19:04:50 -0400

    init

---
+++ cpp/chat_server/chat_server/chat_server/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  chat_server
+//
+//  Created by Shawn Campbell on 7/25/13.
+//  Copyright (c) 2013 Shawn Campbell. All rights reserved.
+//
+
+#include <iostream>
+#include <
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
